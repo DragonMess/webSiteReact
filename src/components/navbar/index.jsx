bf6cc3dc5ce2b53f222c3eb5bd53b904c1c9a1ea
Camilo Pineda <kmylop@hotmail.com>
2020-11-15 23:21:28 -0500

    menu nav mobile added

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+import { theme } from '../../theme';
 import Logo from '../logo';
 import { Marginer } from '../marginer';
 
 const NavBarWrapper = styled.div`
-  position:fixed;
+  /* position:fixed; */
   top:0;
   width:100%;
   min-height:7vh;
-  z-index:1;
-  /* background-color:#3a3939; */
+  /* z-index:1; */
+  background-color:#3a3939;
   display:flex;
   justify-content:space-between;
 `;
@@ -18,17 +21,35 @@ const BrandWrapper = styled.div`
   display:flex;
   flex-direction:row;
 `;
-const LinksWrapper = styled(Link)`
+const LinksWrapper = styled.div`
   min-width:500px;
   display:flex;
   flex-direction:row;
   justify-content:space-between;
   align-items:center;
   text-align:center;
+  @media screen and (max-width:960){
+    display:flex;
+    flex-wrap:wrap;
+    flex-direction:column;
+    width:100%;
+    height:90vh;
+    position:absolute;
+    top:80px;
+    left:${({click}) => (click ? 0 : "-100%")};
+    opacity:1;
+    transition: all .5s ease;
+    background-color:${theme.logo}
+  }
 `;
-const LinksTxt = styled.div`
+const LinksTxt = styled(Link)`
   color:white;
   font-size:calc(2px + 1.2vh);
+  text-decoration:none;
+  @media screen and (max-width:960px){
+    widht:100%;
+    display:table;
+  }
 `;
 const MobileIcon = styled.div`
   display:none;
@@ -44,19 +65,27 @@ const MobileIcon = styled.div`
 `;
 
 function Navbar(porps) {
+
+  const [click , setClick] =useState(false);
+
+  const handleClick = ()=> {
+    setClick(!click);
+  }
   return (
     <NavBarWrapper>
       <Marginer direction="horizontal" margin="0" />
       <BrandWrapper>
         <Logo inLine ></Logo>
       </BrandWrapper>
-      <LinksWrapper>
-        <LinksTxt>SERVICES</LinksTxt>
+        <MobileIcon onClick={handleClick}>{click? <FontAwesomeIcon icon={faBars} /> : <FontAwesomeIcon icon={faTimes} />}
+        </MobileIcon>
+      <LinksWrapper onClick={handleClick}>
+        <LinksTxt to="/services">SERVICES</LinksTxt>
         <LinksTxt>SOUMISSION</LinksTxt>
         <LinksTxt>À PROPOS</LinksTxt>
         <LinksTxt>CONTACT</LinksTxt>
       </LinksWrapper>
-      <MobileIcon>jljsdf</MobileIcon>
+
       <Marginer direction="horizontal" margin="0" />
     </NavBarWrapper>
       
