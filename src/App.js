b9d80f2effed9dd47bb0f008d8d34d3be02ea8c6
Camilo Pineda <kmylop@hotmail.com>
2020-11-15 22:18:50 -0500

    added react-router-dom, added somes routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Homepage from "./containers/homepage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <Homepage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/contact" component="" />
+        <Route path="/soumission" component="" />
+        <Route path="/a_propos" component="" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
