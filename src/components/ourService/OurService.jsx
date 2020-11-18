98f407fca2c2184da03e385cecaad8e1f119b2ff
Camilo Pineda <kmylop@hotmail.com>
2020-11-17 23:26:39 -0500

    added image background and description to ourService component

--- src/components/ourService/OurService.jsx
+++ src/components/ourService/OurService.jsx
@@ -6,19 +6,12 @@ import Office  from '../../assets/pictures/Offices.jpg'
 const ServiceWrap = styled.section`
   margin-left:auto;
   margin-right:auto;
-  padding-left:3vh;
   display:flex;
   flex-direction:row;
   width:70%;
   min-height:30vh;
   margin-bottom:10vh;
     background-size: contain;
-  /* background-color:rgba(55,55,55,0.5); */
-  border-radius:2vh;
-  /* opacity: 0.9; */
-  &:hover{
-    /* background:green; */
-  }
   @media screen and (max-width:960px){
     display:flex;
     flex-direction:column;
@@ -30,35 +23,42 @@ const Service = styled.div`
   display:flex;
   flex-direction:column;
   max-width:100vw;
-
+    @media screen and (max-width:960px){
+    margin-left:auto;
+    margin-right:auto;
+    width:90vw;
+    height:40vh;
+    border-radius:0;
+  }
 `;
 const Title = styled.h3`
   font-size:4vh;
   font-weight:500;
   text-align:center;
-  /* margin:0; */
 `;
 const Description = styled.p`
   font-size:3vh;
   font-weight:300;
   text-align:center;
   color:#7A7A7A;
-  color:white;
   margin:0;
 `;
 const ServiceImg = styled.div`
-  border-radius:2vh;
-  /* display:fex; */
   min-width:30vw;
   background:url('${Office}') no-repeat right;
   background-size: contain;
   opacity: 0.7;
+  margin-left:2vh;
   @media screen and (max-width:960px){
     margin-left:auto;
     margin-right:auto;
     width:90vw;
     height:40vh;
     border-radius:0;
+    background:url('${Office}') no-repeat center;
+    background-size: contain;
+    margin-left:0;
+
   }
 `;
 
