4efc3b3ab05557ee9ee85d8532e43deadd61bcb5
Camilo Pineda <kmylop@hotmail.com>
2020-12-08 09:25:13 -0500

    modified font size and dimentions in footer componnet

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -11,18 +11,21 @@ const FooterWrapper = styled.div`
    flex-direction:column;
    align-items:center;
    justify-content:center;
-   padding:1%;
+   /* padding:.0 0 0 0; */
    margin:0 0 -18vh 0 ;
 
    @media screen and (max-width:960px){
     padding-top:0;
    }
 `;
-const Copyrigth = styled.p`
-  font-size:1.5vh;
+const SloganFooter= styled.p`
+  font-size:2.5vh;
   margin:0;
 `;
-
+const Copyrigth = styled.p`
+  font-size:1.2vh;
+  margin:0;
+`;
 const LinkDev = styled.a`
   color:#ffff;
 `;
@@ -31,9 +34,9 @@ function Footer(props) {
   return (
     <FooterWrapper>
       <Logo inLine={"footer"} ></Logo>
-      <p>À votre disposition pour tous vos besoins</p>
-  <Copyrigth>© 2020 Copyright: <LinkDev href="https://portfolio-camilo-pineda.herokuapp.com/">CamiloPinedaDev</LinkDev></Copyrigth>
-    <Copyrigth>Created by: DragonMess </Copyrigth>
+      <SloganFooter>À votre disposition pour tous vos besoins</SloganFooter>
+  <Copyrigth>© 2020 Copyright: <LinkDev href="https://portfolio-camilo-pineda.herokuapp.com/">CamiloPinedaDev</LinkDev> -- Created by: DragonMess </Copyrigth>
+    <Copyrigth></Copyrigth>
     </FooterWrapper>
   )
 }
